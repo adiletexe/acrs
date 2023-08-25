524a7c05d7a1547fb65d6b72c6be22c362e31a1e
adiletexe <108264958+adiletexe@users.noreply.github.com>
2023-08-25 10:24:14 +0600

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Containers, Position, Trash
-# Register your models here.
-
-admin.site.register(Containers)
-admin.site.register(Position)
-admin.site.register(Trash)
-
